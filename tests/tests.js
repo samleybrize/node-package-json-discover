6389f5ba00af5509d4370cf08650ece1e3361f46
Stephen Berquet <stephen.berquet@gmail.com>
2015-04-30 21:01:23 +0200

    added path cache

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,14 @@
 var packageJsonDiscover = require("..");
 var chai                = require("chai");
 var path                = require("path");
+var proxyquire          = require('proxyquire').noPreserveCache();
 
+// TODO test cache
 describe("package-json-discover", function() {
+    beforeEach("Clear cache path", function() {
+        packageJsonDiscover.destroyCache();
+    });
+
     describe(".discover()", function() {
         it("found with 'from' arg", function() {
             var expectedPath = path.resolve(path.join(__dirname, "../package.json"));
@@ -27,6 +33,42 @@ describe("package-json-discover", function() {
         it("no package.json found", function() {
             var p = path.resolve(__dirname, "../..");
             chai.expect(packageJsonDiscover.discover.bind(packageJsonDiscover, "../..")).to.throw("Unabe to find a package.json from '" + p + "'");
+        });
+
+        it("use path cache", function() {
+            var fsExistsIgnore  = null;
+            var disableFs       = false;
+            var fsStub          = {
+                existsSync: function(path) {
+                    if (disableFs && path !== fsExistsIgnore) {
+                        throw new Error("fs.existsSync() has been called");
+                    } else {
+                        return require("fs").existsSync(path);
+                    }
+                },
+                lstatSync: function(path) {
+                    return require("fs").lstatSync(path);
+                },
+                "@noCallThru": true
+            };
+            var packageJsonDiscoverStubbed = proxyquire("..", {
+                fs: fsStub
+            });
+
+            // fill the path cache
+            var inputDir1       = __dirname + "/from/sub/dir";
+            var inputDir2       = path.dirname(__dirname);
+            var expectedPath    = path.resolve(path.join(__dirname, "../package.json"));
+            chai.expect(packageJsonDiscoverStubbed.discover(inputDir1)).to.equal(expectedPath);
+
+            // disable fs.existsSync() that is used
+            disableFs       = true;
+            fsExistsIgnore  = inputDir1;
+            chai.expect(packageJsonDiscoverStubbed.discover(inputDir1)).to.equal(expectedPath);
+            chai.expect(packageJsonDiscoverStubbed.discover("from/sub/dir")).to.equal(expectedPath);
+
+            fsExistsIgnore  = inputDir2;
+            chai.expect(packageJsonDiscoverStubbed.discover("..")).to.equal(expectedPath);
         });
     });
 
